926d8b45024934e05032a7845bfa02f3a2c77e81
Oldiee_ <marcospradofelix@gmail.com>
2024-03-19 14:22:19 -0300

    primeiro commit

---
+++ src/main/java/br/com/marcosprado/sistemadepagamentos/SistemaDePagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemadepagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePagamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDePagamentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/marcosprado/sistemadepagamentos/SistemaDePagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemadepagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePagamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
